8f8cc54d17897884e7b38c52c16c9f7e24f3c917
jesseowenuk <jesse@acornengine.org>
2024-08-04 20:44:12 +0100

    VGA: Added VGA enumeraton

--- kernel_entry.c
+++ kernel_entry.c
@@ -4,6 +4,25 @@
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
 
+enum vga_colour {
+    VGA_COLOUR_BLACK =          0,
+    VGA_COLOUR_BLUE =           1,
+    VGA_COLOUR_GREEN =          2,
+    VGA_COLOUR_CYAN =           3,
+    VGA_COLOUR_RED =            4,
+    VGA_COLOUR_MAGENTA =        5,
+    VGA_COLOUR_BROWN =          6,
+    VGA_COLOUR_LIGHT_GREY =     7,
+    VGA_COLOUR_DARK_GREY =      8,
+    VGA_COLOUR_LIGHT_BLUE =     9,
+    VGA_COLOUR_LIGHT_GREEN =    10,
+    VGA_COLOUR_LIGHT_CYAN =     11,
+    VGA_COLOUR_LIGHT_RED =      12, 
+    VGA_COLOUR_LIGHT_MAGENTA =  13,
+    VGA_COLOUR_LIGHT_BROWN =    14,
+    VGA_COLOUR_WHITE =          15
+};
+
 uint16_t* vga_buffer;
 
 void kernel_entry(void)
