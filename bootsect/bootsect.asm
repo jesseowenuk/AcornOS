2649573d66a249e94f4ddba0700f62d4e95e1c35
Jesse Owen <jesseowenuk@googlemail.com>
2026-03-26 15:14:32 +0000

    Moved disk loading to its own file

--- bootsect/bootsect.asm
+++ bootsect/bootsect.asm
@@ -29,50 +29,10 @@ end:
     hlt
 
 ;
-; load_sector
-; Loads a single sector from disk using CHS addressing
-;
-; IN
-;   ES = Segment of where to put the buffer
-;   BX = Offset of where to put the buffer (together these become ES:BX)
-;   DL = drive (BIOS will put the value we need in here)
-;
-; OUT
-;   Carry set on error
-;
-; BIOS Reading Sectors (CHS)
-;   AH = 0x02
-;   AL = total sector count to read (must be no more than 127 and cannnot be 0)
-;   CH = cylinder (0-1023)
-;   CL = First sector to read
-;   DH = Head (0-15)
-;   ES:BX = where to put the bytes read
-;   DL = drive number (BIOS will provide this already provides this in the right place :))
-;   int 0x13
-load_sector:
-    push ax
-    push bx
-    push cx
-    push dx
-
-    mov ah, 0x02
-    mov al, 1
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
-    int 0x13
-
-    .done:
-        pop dx
-        pop cx
-        pop bx
-        pop ax
-        ret
-
-;
 ; Real Mode Includes *************************************************************
 ;
 %include 'print_string.asm'
+%include 'disk_load.asm'
 
 ;
 ; Data ***************************************************************************
---
+++ bootsect/disk_load.asm
@@ -0,0 +1,45 @@
+;
+; disk_load.asm
+; Real mode disk loading functions
+;
+
+;
+; load_sector
+; Loads a single sector from disk using CHS addressing
+;
+; IN
+;   ES = Segment of where to put the buffer
+;   BX = Offset of where to put the buffer (together these become ES:BX)
+;   DL = drive (BIOS will put the value we need in here)
+;
+; OUT
+;   Carry set on error
+;
+; BIOS Reading Sectors (CHS)
+;   AH = 0x02
+;   AL = total sector count to read (must be no more than 127 and cannnot be 0)
+;   CH = cylinder (0-1023)
+;   CL = First sector to read (starts from 1!)
+;   DH = Head (0-15)
+;   ES:BX = where to put the bytes read
+;   DL = drive number (BIOS will provide this already provides this in the right place :))
+;   int 0x13
+load_sector:
+    push ax
+    push bx
+    push cx
+    push dx
+
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    int 0x13
+
+    .done:
+        pop dx
+        pop cx
+        pop bx
+        pop ax
+        ret
