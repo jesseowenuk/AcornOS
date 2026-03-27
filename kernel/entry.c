783559552f1e827119ec3f8b8e9ea5e1627a164b
Jesse Owen <jesseowenuk@googlemail.com>
2026-03-27 23:12:19 +0000

    Eeep! Successfully running code from C! Okay, we are so not in the right place in memory but this can wait to sort - we've written an 'A' to the screen in a green colour and it's from C code :-)

--- kernel/entry.c
+++ kernel/entry.c
@@ -4,7 +4,16 @@
 // Our kernel C entry point
 //
 
+void test()
+{
+    char *vga_memory = (char *)0xb8000;
+
+    vga_memory[0] = 'A';
+    vga_memory[1] = 0x0a;
+}
+
 int main()
 {
+    test();
     return 0; 
 }
