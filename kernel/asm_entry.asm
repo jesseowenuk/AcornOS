13a93930b808b3f3b05086f729354e038ae16f62
Jesse Owen <jesseowenuk@googlemail.com>
2026-03-28 10:47:02 +0000

    Starting to implement meaty skeleton structure * reserving space for the stack

--- kernel/asm_entry.asm
+++ kernel/asm_entry.asm
@@ -7,9 +7,17 @@
 bits 64
 extern main
 
+section .bss
+align 16
+stack_bottom:
+buffer resb 16386     ; reserves 16 KiB for the stack
+stack_top:
+
 section .text
 global _start
 
 _start:
+    mov rsp, stack_top
+
     call main
     hlt
