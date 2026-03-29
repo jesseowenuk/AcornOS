cb1438a0e0d2bba8b7657fd18c9e2407336ec277
Jesse Owen <jesseowenuk@googlemail.com>
2026-03-29 21:37:38 +0100

    [BOOTLOADER] Added initialisation code

--- bootloader/bootsector/bootsector.asm
+++ bootloader/bootsector/bootsector.asm
@@ -1,6 +1,23 @@
 org 0x7C00
 bits 16
 
+code_start:
+    ; disable interrupts with cli so we don't get interrupted and jump to 0x0000 
+    cli
+    jmp 0x0000:initialise_cs
+
+initialise_cs:
+    ; set all the segments to 0x0000 and the stack pointer to the start of the bootloader code,
+    ; this grows downwards so is fine here. Finally we re-enable interrupts with sti
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    sti
+
 halt:
     hlt
     jmp halt
