6e49cbec82a1094b53f2342e584c4edbec8953ed
Jesse Owen <jesseowenuk@googlemail.com>
2026-04-04 21:06:35 +0100

    [ECHFS] Add file size calculation

--- echfs/echfs-utils.c
+++ echfs/echfs-utils.c
@@ -5,6 +5,7 @@
 static int verbose = 0;
 
 static FILE* image;
+static uint64_t image_size;
 
 int main(int argc, char **argv)
 {
@@ -36,7 +37,18 @@ int main(int argc, char **argv)
     if((image = fopen(argv[1], "r+")) == NULL)
     {
         fprintf(stderr, "%s: error: couldn't access `%s`.\n", argv[0], argv[1]);
+        return EXIT_FAILURE;
     }
+
+    // Calculate the size of the file provided.
+    // Move the file pointer to the end of the file
+    fseek(image, 0L, SEEK_END);
+
+    // Use ftell to read the current position in the file
+    image_size = (uint64_t)ftell(image);
+
+    // rewind the file pointer back to the start of the file
+    rewind(image);
 
     return EXIT_SUCCESS;
 }
