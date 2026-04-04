d8ecfc76a06d2ea2e3fb2752b6d1d4b0eeb3aff6
Jesse Owen <jesseowenuk@googlemail.com>
2026-04-04 18:33:53 +0100

    [LIBC] Implemented strcmp function

--- bootloader/lib/libc.c
+++ bootloader/lib/libc.c
@@ -3,17 +3,30 @@
 
 #include <lib/libc.h>
 
-void *memcpy(void *destination, const void *source, size_t count)
+void *memcpy(void *destination, const void *source, size_t string_length)
 {
     size_t i = 0;
 
     uint8_t *destination_temp = destination;
     const uint8_t *source_temp = source;
 
-    for(i = 0; i < count; i++)
+    for(i = 0; i < string_length; i++)
     {
         destination_temp[i] = source_temp[i];
     }
 
     return destination;
+}
+
+int strcmp(const char *string_one, const char *string_two, size_t string_length)
+{
+    size_t i;
+
+    for(i = 0; i < string_length; i++)
+    {
+        if(string_one[i] != string_two[i])
+        {
+            return 1;
+        }
+    }
 }
--- bootloader/lib/libc.h
+++ bootloader/lib/libc.h
@@ -4,5 +4,6 @@
 #include <stddef.h>
 
 void *memcpy(void *destination, const void *source, size_t count);
+int strcmp(const char *string_one, const char *string_two);
 
 #endif
